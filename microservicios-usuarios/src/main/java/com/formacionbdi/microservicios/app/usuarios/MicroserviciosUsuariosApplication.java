df91d378b764b69a595e2b9e63c4102369e95b75
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2022-07-08 03:27:28 -0500

    version 1.17

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,9 +2,9 @@ package com.formacionbdi.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosUsuariosApplication {
 
