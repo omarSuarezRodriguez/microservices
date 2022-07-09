a53812209014d0b262fb8616b147ed220e4ce13e
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2022-07-08 19:19:43 -0500

    version 1.26

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,15 +2,17 @@ package com.formacionbdi.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.microservicios.commons.alumnos.models.entity"})
+
 public class MicroserviciosUsuariosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviciosUsuariosApplication.class, args);
-		//System.out.println("Hola Mundosss");
+		SpringApplication.run(MicroserviciosUsuariosApplication.class, args);	
 	}
 
 }
--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -4,7 +4,7 @@ package com.formacionbdi.microservicios.app.usuarios.services;
 import org.springframework.stereotype.Service;
 
 
-import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.usuarios.models.repository.AlumnoRepository;
 import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 
