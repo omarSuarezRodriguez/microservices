4ceb417518f9ae38a5ac8f31fbc4e387ee02804c
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2022-07-13 23:12:49 -0500

    version 1.342

--- microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
+++ microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -1,5 +1,7 @@
 package com.formacionbdi.microservicios.app.examenes.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.examenes.models.entity.Examen;
+import com.formacionbdi.microservicios.app.examenes.models.entity.Pregunta;
 import com.formacionbdi.microservicios.app.examenes.services.ExamenService;
 import com.formacionbdi.microservicios.commons.controllers.CommonController;
 
@@ -28,6 +31,20 @@ public class ExamenController extends CommonController<Examen, ExamenService>{
 		examenDb.setNombre(examen.getNombre());
 		
 		
+		// Lista de preguntas eliminadas
+		List<Pregunta> eliminadas = new ArrayList<>();
+		
+		examenDb.getPreguntas().forEach(pdb -> {
+			if(!examen.getPreguntas().contains(pdb)) {
+				eliminadas.add(pdb);
+				
+			}
+		});
+		
+		
+		eliminadas.forEach(p -> {
+			examenDb.removePregunta(p);
+		});
 		
 		
 		return null;
