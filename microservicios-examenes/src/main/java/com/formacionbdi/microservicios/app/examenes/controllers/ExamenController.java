c3e88edfe175f5c007c24608451bef26c8270e60
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2022-07-18 04:11:36 -0500

    version 1.35

--- microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
+++ microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -1,9 +1,8 @@
 package com.formacionbdi.microservicios.app.examenes.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.examenes.models.entity.Examen;
-import com.formacionbdi.microservicios.app.examenes.models.entity.Pregunta;
 import com.formacionbdi.microservicios.app.examenes.services.ExamenService;
 import com.formacionbdi.microservicios.commons.controllers.CommonController;
 
@@ -29,17 +27,20 @@ public class ExamenController extends CommonController<Examen, ExamenService> {
 
 		Examen examenDb = o.get();
 		examenDb.setNombre(examen.getNombre());
+		
+		
+		// Forma 1 para eliminar preguntas repetidas
+		
+		// Usando stream de java 8
+		examenDb.getPreguntas()
+		.stream()
+		.filter(pdb -> !examen.getPreguntas().contains(pdb))
+		.forEach(examenDb::removePregunta);
+		
 
-		// Lista de preguntas eliminadas.
-		List<Pregunta> eliminadas = new ArrayList<>();
-
-		examenDb.getPreguntas().forEach(pdb -> {
-			if (!examen.getPreguntas().contains(pdb)) {
-				eliminadas.add(pdb);
-
-			}
-		});
-
+		
+		// Forma 2 para eliminar pregunas repetidas
+		
 //		// Lista de preguntas eliminadas.
 //		List<Pregunta> eliminadas = new ArrayList<>();
 //
@@ -49,16 +50,21 @@ public class ExamenController extends CommonController<Examen, ExamenService> {
 //
 //			}
 //		});
-
-		eliminadas.forEach(p -> {
-			examenDb.removePregunta(p);
-		});
-
+//
+//		// Eliminando preguntas repetidas manera corta
+//		eliminadas.forEach(examenDb::removePregunta);
+		
+//		// Eliminando preguntas repetidas
 //		eliminadas.forEach(p -> {
 //			examenDb.removePregunta(p);
 //		});
+		
+		
+		
+		examenDb.setPreguntas(examen.getPreguntas());
 
-		return null;
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDb));
 
 	}
 
